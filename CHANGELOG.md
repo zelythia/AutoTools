f6fca23765a1fbdb21a8b8fb86643ef353734b33
lukas <dev@zelythia.net>
2025-09-25 16:49:33 +0200

    v3.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,7 @@
-- Added Controllable Support
-- Fixed Minecraft version requirements
+**Warning: This update changes the config file location and will reset your configurations**
+- Added config screen option to modify Silk Touch, Fortune and Shears block list to allow for further configuration
+- Custom tools now also support block and item tags + Better editing menu in config screen
+
+
+- Fixed a bug where shears would always be prioritized over tools with fortune or silk touch
+- Fixed wrong localisation for SilkTouch Setting keybind
--- common/src/main/java/net/zelythia/TooltipHelper.java
+++ common/src/main/java/net/zelythia/TooltipHelper.java
@@ -10,13 +10,14 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.zelythia.config.AutoToolsConfig;
 
 import java.util.List;
 
 public class TooltipHelper {
 
     public static void applyTooltip(ItemStack stack, List<Component> tooltip) {
-        if (AutoToolsConfig.SHOWDPS) {
+        if (AutoToolsConfig.get().showDPS) {
             Item item = stack.getItem();
 
             if (item != Items.AIR) {
