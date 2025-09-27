564afa57b4865c886b043e927ada508729a8bdcf
lukas <dev@zelythia.net>
2025-09-27 19:09:19 +0200

    Fix wrong package name

---
+++ common/src/main/java/net/zelythia/autotools/TooltipHelper.java
@@ -0,0 +1,83 @@
+package net.zelythia.autotools;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.zelythia.autotools.config.AutoToolsConfig;
+
+import java.util.List;
+
+public class TooltipHelper {
+
+    public static void applyTooltip(ItemStack stack, List<Component> tooltip) {
+        if (AutoToolsConfig.get().showDPS) {
+            Item item = stack.getItem();
+
+            if (item != Items.AIR) {
+                double attackDamage = 0;
+                double attackSpeed = 0;
+
+                attackDamage = stack.getDamageValue();
+
+                for (ItemAttributeModifiers.Entry modifier : stack.get(DataComponents.ATTRIBUTE_MODIFIERS).modifiers()) {
+                    if(modifier.attribute().is(Attributes.ATTACK_DAMAGE)){
+                        attackDamage = modifier.modifier().amount();
+                    }
+                    else if(modifier.attribute().is(Attributes.ATTACK_SPEED)){
+                        attackSpeed = modifier.modifier().amount();
+                    }
+                }
+
+                if(attackDamage > 0){
+                    if(attackSpeed > 0){
+                        attackDamage = (1 + attackDamage) * (4F + attackSpeed);
+                    }
+                    else{
+                        attackDamage = attackDamage + 1;
+                    }
+                }
+//
+                double optionalAttackDamage = attackDamage;
+                //Check for enchantments
+                if (stack.isEnchanted()) {
+                    attackDamage += EnchantmentHelper.getDamageBonus(stack, null);
+
+                    if (optionalAttackDamage + EnchantmentHelper.getDamageBonus(stack, EntityType.ZOMBIE) > attackDamage) {
+                        optionalAttackDamage += EnchantmentHelper.getDamageBonus(stack, EntityType.ZOMBIE);
+                    } else if (optionalAttackDamage + EnchantmentHelper.getDamageBonus(stack, EntityType.SPIDER) > attackDamage) {
+                        optionalAttackDamage += EnchantmentHelper.getDamageBonus(stack, EntityType.SPIDER);
+                    } else if (optionalAttackDamage + EnchantmentHelper.getDamageBonus(stack, EntityType.GUARDIAN) > attackDamage) {
+                        optionalAttackDamage += EnchantmentHelper.getDamageBonus(stack, EntityType.GUARDIAN);
+                    }
+                }
+
+                if (attackDamage > 1) {
+
+                    int index = 0;
+                    for (int i = tooltip.size() - 1; i >= 0; i--) {
+                        if(tooltip.get(i).getStyle().getColor() != null){
+                            if(tooltip.get(i).getStyle().getColor().getValue() == 43520){
+                                index = i;
+                                break;
+                            }
+                        }
+                    }
+                    if (index < tooltip.size()) index++;
+
+                    String damage = (optionalAttackDamage > attackDamage) ?
+                            (double) Math.round(attackDamage * 10d) / 10d + " (" + (double) Math.round(optionalAttackDamage * 10d) / 10d + ")" :
+                            String.valueOf((double) Math.round(attackDamage * 10d) / 10d);
+
+                    tooltip.add(index, Component.literal(" " + damage + " Dps").withStyle(ChatFormatting.DARK_GREEN));
+                }
+            }
+        }
+    }
+}
---
+++ fabric/src/main/java/net/zelythia/autotools/fabric/AutoToolsFabric.java
@@ -0,0 +1,104 @@
+package net.zelythia.autotools.fabric;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.gui.registry.GuiRegistry;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.zelythia.autotools.AutoTools;
+import net.zelythia.autotools.TooltipHelper;
+import net.zelythia.autotools.config.AutoToolsConfig;
+import net.zelythia.autotools.config.autoconfig.BlockList;
+import net.zelythia.autotools.config.autoconfig.BlockListAnnotationProvider;
+import net.zelythia.autotools.config.autoconfig.CustomToolsTransformer;
+import org.lwjgl.glfw.GLFW;
+
+public class AutoToolsFabric implements ClientModInitializer {
+    private boolean keyPressed = false;
+
+    private static final KeyMapping KEY_AUTOTOOLS = KeyBindingHelper.registerKeyBinding(new KeyMapping("key.autotools.get_tool", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, "key.autotools.category"));
+    private static final KeyMapping KEY_SILKTOUCH = KeyBindingHelper.registerKeyBinding(new KeyMapping("key.autotools.silktouch", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z, "key.autotools.category"));
+
+    @Override
+    public void onInitializeClient() {
+        AutoConfig.register(net.zelythia.autotools.config.AutoToolsConfig.class, PartitioningSerializer.wrap(GsonConfigSerializer::new));
+
+        AutoConfig.getConfigHolder(net.zelythia.autotools.config.AutoToolsConfig.class).registerSaveListener((configHolder, autoToolsConfig) -> {
+            AutoTools.reloadConfig();
+            return InteractionResult.SUCCESS;
+        });
+
+        GuiRegistry registry = AutoConfig.getGuiRegistry(net.zelythia.autotools.config.AutoToolsConfig.class);
+        registry.registerAnnotationProvider(new BlockListAnnotationProvider(), BlockList.class);
+        registry.registerPredicateTransformer(new CustomToolsTransformer(), field -> field.getName().equals("customTools"));
+
+        AutoTools.init();
+
+        ClientTickEvents.START_CLIENT_TICK.register(client -> {
+            if (AutoToolsConfig.get().toggle) {
+                //Changing the toggle setting:
+                //When toggling the keybinding should only be reacted to once per press
+                if (KEY_AUTOTOOLS.consumeClick()) {
+                    if (!keyPressed) {
+                        AutoTools.toggle = !AutoTools.toggle;
+                        client.player.sendSystemMessage(AutoTools.toggle ? Component.translatable("chat.enabled_autotools") : Component.translatable("chat.disabled_autotools"));
+                        keyPressed = true;
+                    }
+                    //resetting the keyPressed-count
+                    while (KEY_AUTOTOOLS.consumeClick()) {
+                        keyPressed = true;
+                    }
+                } else {
+                    keyPressed = false;
+                }
+            } else {
+                if (KEY_AUTOTOOLS.consumeClick()) {
+                    AutoTools.startedMining = false;
+                    AutoTools.getCorrectTool(client.hitResult, client);
+                }
+            }
+        });
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (AutoToolsConfig.get().switchBack) {
+                if (Minecraft.getInstance().options.keyAttack.isDown()) {
+                    AutoTools.startedMining = true;
+                } else {
+                    //Detecting switchBack for entities when using toggle, switching back otherwise if the key is released
+                    if (AutoToolsConfig.get().toggle || AutoTools.startedMining) {
+                        AutoTools.switchBack();
+                    }
+                }
+            }
+
+            if(KEY_SILKTOUCH.consumeClick()) {
+                AutoToolsConfig.PreferSilkTouch[] values = AutoToolsConfig.PreferSilkTouch.values();
+                AutoToolsConfig.get().preferSilkTouch = values[(AutoToolsConfig.get().preferSilkTouch.ordinal() + 1) % values.length];
+
+                client.player.displayClientMessage(Component.translatable("chat.cycle_silktouch").append(Component.translatable("text.autoconfig.autotools.option.general.preferSilkTouch." + AutoToolsConfig.get().preferSilkTouch)), false);
+
+                AutoToolsConfig.save();
+            }
+        });
+
+
+        ItemTooltipCallback.EVENT.register((stack, tooltipContext, tooltipType, lines) -> {
+            TooltipHelper.applyTooltip(stack, lines);
+        });
+
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            AutoTools.swaps.clear();
+        });
+    }
+
+
+}
